0393c06dcc6ae44f0059dc3d06b4e4a2cfe74fd0
jojonki <junki.ohmura@gmail.com>
2017-12-04 13:52:24 -0500

    add GRU

--- pointer_network.py
+++ pointer_network.py
@@ -6,7 +6,7 @@ from utils import to_var
 
 
 class PointerNetwork(nn.Module):
-    def __init__(self, input_size, emb_size, weight_size, seq_len, answer_seq_len, hidden_size=512):
+    def __init__(self, input_size, emb_size, weight_size, seq_len, answer_seq_len, hidden_size=512, is_GRU=False):
         super(PointerNetwork, self).__init__()
 
         self.hidden_size = hidden_size
@@ -15,15 +15,19 @@ class PointerNetwork(nn.Module):
         self.weight_size = weight_size
         self.seq_len = seq_len
         self.emb_size = emb_size
+        self.is_GRU = is_GRU
 
         self.emb = nn.Embedding(input_size, emb_size)  # embed inputs
-        self.enc = nn.LSTM(emb_size, hidden_size, batch_first=True)
-        self.dec = nn.LSTMCell(emb_size, hidden_size)
-        self.W1 = nn.Linear(hidden_size, weight_size) # blending encoder
-        self.W2 = nn.Linear(hidden_size, weight_size) # blending decoder
-        self.vt = nn.Linear(weight_size, 1) # scaling sum of enc and dec by v.T
+        if is_GRU:
+            self.enc = nn.GRU(emb_size, hidden_size, batch_first=True)
+            self.dec = nn.GRUCell(emb_size, hidden_size) # GRUCell's input is always batch first
+        else:
+            self.enc = nn.LSTM(emb_size, hidden_size, batch_first=True)
+            self.dec = nn.LSTMCell(emb_size, hidden_size) # LSTMCell's input is always batch first
 
-        self.tanh = nn.Tanh()
+        self.W1 = nn.Linear(hidden_size, weight_size, bias=False) # blending encoder
+        self.W2 = nn.Linear(hidden_size, weight_size, bias=False) # blending decoder
+        self.vt = nn.Linear(weight_size, 1, bias=False) # scaling sum of enc and dec by v.T
 
     def forward(self, input):
         batch_size = input.size(0)
@@ -34,19 +38,22 @@ class PointerNetwork(nn.Module):
         encoder_states = encoder_states.transpose(1, 0) # (L, bs, H)
 
         # Decoding states initialization
-        decoder_input = to_var(torch.Tensor(batch_size, self.emb_size).zero_()) # (bs, embd_size)
-        hidden = to_var(torch.randn([batch_size, self.hidden_size]))            # (bs, h)
-        cell_state = encoder_states[-1]                                         # (bs, h)
+        decoder_input = to_var(torch.zeros(batch_size, self.emb_size)) # (bs, embd_size)
+        hidden = to_var(torch.zeros([batch_size, self.hidden_size]))   # (bs, h)
+        cell_state = encoder_states[-1]                                # (bs, h)
 
         probs = []
         # Decoding
         for i in range(self.answer_seq_len): # range(M)
-            hidden, cell_state = self.dec(decoder_input, (hidden, cell_state)) # (bs, h), (bs, h)
+            if self.is_GRU:
+                hidden = self.dec(decoder_input, hidden) # (bs, h), (bs, h)
+            else:
+                hidden, cell_state = self.dec(decoder_input, (hidden, cell_state)) # (bs, h), (bs, h)
 
             # Compute blended representation at each decoder time step
             blend1 = self.W1(encoder_states)          # (L, bs, W)
             blend2 = self.W2(hidden)                  # (bs, W)
-            blend_sum = self.tanh(blend1 + blend2)    # (L, bs, W)
+            blend_sum = F.tanh(blend1 + blend2)    # (L, bs, W)
             out = self.vt(blend_sum).squeeze()        # (L, bs)
             out = F.log_softmax(out.t().contiguous()) # (bs, L)
             probs.append(out)
